faa990052e6ea7a8bb18a344ee9623f010dba0a2
thiagovb46 <thiagovb46@gmail.com>
2022-03-07 22:30:30 -0300

    First Commit

---
+++ realtimetodo.client/src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <h1>To DO List</h1>
+    <br />
+    <table>
+      <thead>
+        <tr>
+          <th>List Name</th>
+          <th>Peddings</th>
+          <th>Completed</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="l in lists" :key="l.id">
+          <td>
+            <router-view :to="{name:'list', params:{listId: l.id}}"> {{l.listName}} </router-view>
+            </td>
+          <td>{{l.completed}}</td>
+          <td>{{l.pending}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+
+@Component
+export default class HomeView extends Vue {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  lists: any[] = [];
+
+  demoList(){
+    this.lists.push({id: 0, listName:"Foo", completed:5, pending: 10});
+    this.lists.push({id: 1, listName:"bar", completed:5, pending: 10});
+    this.lists.push({id: 2, listName:"Foo", completed:5, pending: 10});
+    this.lists.push({id: 3, listName:"Foo", completed:5, pending: 10});
+  }
+  created(){
+    this.demoList();
+  }
+}
+</script>
